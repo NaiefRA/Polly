494621602c4217f7e67bec3b6a75b0e64f285296
knife <ashrafnaiefryan@gmail.com>
2025-01-31 20:01:40 +0530

    Started Create Poll

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <div id="navbar">
       <h2 className="page-title">Welcome to Polly</h2>
-      <div className="navbar-element">Create Poll</div>
-      <div className="navbar-element">About</div>
+      <Link to="/new-poll" className="navbar-element">
+        Create Poll
+      </Link>
+      <Link to="/" className="navbar-element">
+        About
+      </Link>
     </div>
   );
 };
