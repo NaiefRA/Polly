e5106eee9bd51a315ebae189404e9959375227a0
knife <ashrafnaiefryan@gmail.com>
2025-02-03 11:48:18 +0530

    Adding stuff

--- src/DisplayPolls.js
+++ src/DisplayPolls.js
@@ -1,4 +1,45 @@
 const DisplayPolls = ({ polls }) => {
+  const handleClick = (optionNo) => {
+    async function updateVotes(pollId, optionNumber) {
+      try {
+        const response = await fetch(`http://localhost:3000/Polls/${pollId}`);
+        const poll = await response.json();
+
+        const updatedOptions = poll.options.map((option) => {
+          if (option.optionNumber === optionNumber) {
+            return {
+              ...option,
+              optionVotes: option.optionVotes + 1,
+            };
+          }
+          return option;
+        });
+
+        const updateResponse = await fetch(
+          `http://localhost:3000/Polls/${pollId}`,
+          {
+            method: "PATCH",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              options: updatedOptions,
+            }),
+          }
+        );
+
+        if (!updateResponse.ok) {
+          throw new Error("Failed to update votes");
+        }
+
+        return await updateResponse.json();
+      } catch (error) {
+        console.error("Error updating votes:", error);
+        throw error;
+      }
+    }
+  };
+
   return (
     <div id="polls">
       {polls.map((poll) => {
@@ -9,7 +50,14 @@ const DisplayPolls = ({ polls }) => {
             <div>
               {poll.options.map((option) => (
                 <div id="options" key={option.optionNumber}>
-                  <button id="option-button">{option.optionValue}</button>
+                  <button
+                    id="option-button"
+                    onClick={() => {
+                      handleClick(option.optionNumber);
+                    }}
+                  >
+                    {option.optionValue}
+                  </button>
                   <p id="option-votes">{option.optionVotes}</p>
                 </div>
               ))}
